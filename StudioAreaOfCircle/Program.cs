b4262779b8027a06e3df36b90fe929b6db1d463f
ozlemyalcin <omersam33@gmail.com>
2021-01-05 09:09:13 -0600

    area

---
+++ StudioAreaOfCircle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StudioAreaOfCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a radius: ");
+            string userInput = Console.ReadLine();
+            double radius = double.Parse(userInput);
+
+            double pi = 3.14;
+
+            double areaOfCircle = (pi * radius * radius);
+
+            Console.WriteLine("The area of a circle of radius " + radius + " is: " + areaOfCircle);
+
+
+        }
+    }
+}
